a048fa1ed9ac35f450d2b798974109b08141985c
Yuliia Shybyk <j7557500@gmail.com>
2022-02-13 18:22:37 +0200

    task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const galleryItemsMarkup = galleryItems
+  .map(
+    (item) =>
+      `<a class="gallery__item" href=${item.original}>
+          <img
+            class="gallery__image"
+            src=${item.preview}
+            alt=${item.description}
+          />
+        </a>`
+  )
+  .join("");
+
+galleryList.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+console.log(galleryList);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
